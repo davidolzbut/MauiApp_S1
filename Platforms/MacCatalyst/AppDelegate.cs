8e8eab3776ac108a835e0e469e9e76f4702a13a1
davidolz <davidolzbut@gmail.com>
2024-01-08 10:30:14 +0100

    init

--- Platforms/MacCatalyst/AppDelegate.cs
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace MauiApp_S1;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
