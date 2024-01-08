fad94056085ac34d24ebc00c58c401cf51b62e42
davidolz <davidolzbut@gmail.com>
2024-01-08 11:10:59 +0100

    added pages and routing

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,9 +1,13 @@
 ﻿namespace MauiApp_S1;
+using MauiApp_S1.Views;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+        Routing.RegisterRoute(nameof(FlowersPage), typeof(FlowersPage));
+        Routing.RegisterRoute(nameof(AddFlowerPage), typeof(AddFlowerPage));
+        Routing.RegisterRoute(nameof(EditFlowerPage), typeof(EditFlowerPage));
     }
 }
